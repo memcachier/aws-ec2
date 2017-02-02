67bee1f6374d63e6f6de9d897b86b32267c48101
Ian Ross <iross@cadasta.org>
2017-02-02 17:14:47 +0100

    Stackage update

--- src/Aws/Elb/Commands/DescribeLoadBalancerPolicyTypes.hs
+++ src/Aws/Elb/Commands/DescribeLoadBalancerPolicyTypes.hs
@@ -28,9 +28,9 @@ instance SignQuery DescribeLoadBalancerPolicyTypes where
 
 instance ResponseConsumer DescribeLoadBalancerPolicyTypes Value where
     type ResponseMetadata Value = QueryMetadata
-    responseConsumer ListLoadBalancerPolicyTypes = queryResponseConsumer $ \cu -> do
+    responseConsumer _ ListLoadBalancerPolicyTypes = queryResponseConsumer $ \cu -> do
       let cu' = cu $.// Cu.laxElement "PolicyTypeName" &| (toValue (XMLValueOptions "item") . Cu.node)
       return $ Array $ V.fromList cu'
-    responseConsumer (DescribeLoadBalancerPolicyTypes _) = queryResponseConsumer $ valueConsumerOpt (XMLValueOptions "member") "DescribeLoadBalancerPolicyTypesResult" id
+    responseConsumer _ (DescribeLoadBalancerPolicyTypes _) = queryResponseConsumer $ valueConsumerOpt (XMLValueOptions "member") "DescribeLoadBalancerPolicyTypesResult" id
 
 instance Transaction DescribeLoadBalancerPolicyTypes Value
